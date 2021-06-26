0a5522c5b4dc5cbe58582fdf4fbb2b1e4e2df600
Vikash rai <raivikash174@gmail.com>
2021-06-26 19:12:33 +0530

    first commit

---
+++ app/src/main/java/bawv/app/runtracker/ui/DeclaredPermissionActivity.kt
@@ -0,0 +1,11 @@
+package bawv.app.runtracker.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeclaredPermissionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_declared_permission)
+    }
+}
